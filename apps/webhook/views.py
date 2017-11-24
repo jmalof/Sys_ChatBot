7ce068d2952ae6686b93d7ccbfe970b0c51d7d7f
jmalof <jordymalo@hotmail.com>
2017-11-24 16:26:54 -0500

    n text commit4

--- apps/webhook/views.py
+++ apps/webhook/views.py
@@ -71,7 +71,7 @@ def post_facebook_message(fbid, mensaje_recibido):
     user_details_url = "https://graph.facebook.com/v2.6/%s" % fbid
     user_details_params = {'fields': 'first_name,last_name,profile_pic', 'access_token': TOKEN_ACCESO}
     user_details = requests.get(user_details_url, user_details_params).json()
-    texto_salida = '%s, '+texto_salida % user_details['first_name']
+    texto_salida = '%s, '% user_details['first_name'] + texto_salida
 
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s' % TOKEN_ACCESO
     response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": texto_salida}})
