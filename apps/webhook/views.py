6c374dee15b689a3bc77e72f2d1ef98cfa6878d7
jmalof <jordymalo@hotmail.com>
2017-11-24 17:02:47 -0500

    n text commit4

--- apps/webhook/views.py
+++ apps/webhook/views.py
@@ -58,9 +58,9 @@ def post_facebook_message(fbid, mensaje_recibido):
     for palabra in texto_entrada:
         print("palabra", palabra)
 
-        if Diccionario.objects.filter(palabra_clave__iexact=palabra).exists():
+        if Diccionario.objects.filter(palabra_clave__search=palabra).exists():
             print("existe", palabra)
-            listado = Diccionario.objects.filter(palabra_clave__iexact=palabra)
+            listado = Diccionario.objects.filter(palabra_clave__search=palabra)
             random_index = random.randint(0, listado.count() - 1)
             obj = listado[random_index]
             texto_salida += obj.respuesta
