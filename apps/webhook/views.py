5d2334052c697c4f9c9758a48676b05094bea1bb
jmalof <jordymalo@hotmail.com>
2017-11-24 14:21:32 -0500

    n text commit4

--- apps/webhook/views.py
+++ apps/webhook/views.py
@@ -54,10 +54,11 @@ def post_facebook_message(fbid, mensaje_recibido):
     # Remove all punctuations, lower case the text and split it based on space
     texto_entrada = re.sub(r"[^a-zA-Z0-9\s]", ' ', mensaje_recibido).lower().split()
     texto_salida = ''
-    print(texto_entrada)
+
     for palabra in texto_entrada:
-        if Diccionario.objects.filter(palabra_clave=palabra).exists():
-            listado = Diccionario.objects.filter(palabra_clave=palabra)
+        if Diccionario.objects.filter(palabra_clave__contains=palabra).exists():
+            print(texto_entrada)
+            listado = Diccionario.objects.filter(palabra_clave__contains=palabra)
             random_index = random.randint(0, listado.count() - 1)
             obj = listado[random_index]
             texto_salida = obj.respuesta
